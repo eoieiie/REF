80d2c1aca2549124ef248155dcaab90bbe19dde0
zxcvb2002 <jbyjby3743@naver.com>
2024-05-06 14:57:19 +0900

    [chore] RoomDB gradle configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication) // id("com.android.application")
     alias(libs.plugins.jetbrainsKotlinAndroid) // id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp") // id ("kotlin-kapt")
 }
 
 android {
@@ -38,6 +39,22 @@ android {
 }
 
 dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // RoomDB
+    implementation(libs.room.runtime) // implementation("androidx.room:room-runtime:2.6.1")
+    implementation(libs.room.ktx) // implementation("androidx.room:room-ktx:2.6.1")
+    // To use Kotlin annotation processing tool (KSP) // (kapt)
+    ksp(libs.room.compiler) // ksp("androidx.room:room-compiler:2.6.1") // kapt("androidx.room:room-compiler:2.6.1") // annotationProcessor("androidx.room:room-compiler:2.6.1")
+
     // Retrofit
     implementation(libs.retrofit) // implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation(libs.gson) // implementation("com.squareup.retrofit2:converter-gson:2.11.0")
@@ -49,13 +66,4 @@ dependencies {
     // define any required OkHttp artifacts without version
     implementation(libs.okhttp) // implementation("com.squareup.okhttp3:okhttp") // implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation(libs.loging.interceptor) // implementation("com.squareup.okhttp3:logging-interceptor") // implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,19 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+
+    id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
+}
+
+buildscript {
+    // ext.kotlin_version = "1.9.23" // (id("kotlin-android-extensions"))
+    extra.apply {
+        set("kotlin_version", "1.9.23")
+    }
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath(libs.ksp.gradle.plugin) // classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.23-1.0.20")
+    }
 }
