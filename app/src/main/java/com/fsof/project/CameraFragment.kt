44fe6b1a27fd167bf17e5a44f6bff392b5616690
zxcvb2002 <jbyjby3743@naver.com>
2024-05-22 16:52:47 +0900

    [fix] add correspond package to file path (same)

--- app/src/main/java/com/fsof/project/CameraFragment.kt
+++ app/src/main/java/com/fsof/project/CameraFragment.kt
@@ -1,10 +1,10 @@
 package com.fsof.project
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.fsof.project.databinding.FragmentCameraBinding
 
 class CameraFragment : Fragment() {
