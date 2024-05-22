96597786eccbb5542aee0106bab67a97f6e567ca
zxcvb2002 <jbyjby3743@naver.com>
2024-05-22 14:47:45 +0900

    [add] TFLite & Support libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,13 @@ buildscript {
     }
     repositories {
         google()
+        mavenCentral()
     }
     dependencies {
         classpath(libs.ksp.gradle.plugin) // classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.24-1.0.20")
+        classpath(libs.gradle) // classpath "com.android.tools.build:gradle:8.3.2"
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
     }
 }
