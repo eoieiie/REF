2847db19b2bf2ebad562d75ff64358e5b3854974
eoieiie <turkiyecivciv31@gmail.com>
2024-05-23 20:07:06 +0900

    [fix] 화면 좀 수정하고... splash 만드려다가 에러
    - Replaced Kotlin `synchronized` with Java `synchronized` block for proper synchronization.
    - Added `@Volatile` annotation to `INSTANCE` to ensure visibility across threads.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,9 +16,13 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
     buildFeatures {
         viewBinding = true
+        compose = true
     }
     buildTypes {
         release {
@@ -36,6 +40,14 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
@@ -46,6 +58,13 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,7 +75,11 @@ dependencies {
 
     // RoomDB
     implementation(libs.room.runtime) // implementation("androidx.room:room-runtime:2.6.1")
-    implementation(libs.room.ktx) // implementation("androidx.room:room-ktx:2.6.1")
+    implementation(libs.room.ktx)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest) // implementation("androidx.room:room-ktx:2.6.1")
     // To use Kotlin annotation processing tool (KSP) // (kapt)
     ksp(libs.room.compiler) // ksp("androidx.room:room-compiler:2.6.1") // kapt("androidx.room:room-compiler:2.6.1") // annotationProcessor("androidx.room:room-compiler:2.6.1")
 
