0951a1c3917897064f4c2af25409641774a01beb
zxcvb2002 <jbyjby3743@naver.com>
2024-05-27 23:49:51 +0900

    [fix] SocketTimeoutException

--- app/src/main/java/com/fsof/project/controller/client/NutrientClient.kt
+++ app/src/main/java/com/fsof/project/controller/client/NutrientClient.kt
@@ -1,8 +1,8 @@
 package com.fsof.project.controller.client
 
 import com.fsof.project.controller.service.NutrientsService
-import com.fsof.project.utils.APIs
 import retrofit2.Retrofit
+import com.fsof.project.utils.APIs
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NutrientClient {
