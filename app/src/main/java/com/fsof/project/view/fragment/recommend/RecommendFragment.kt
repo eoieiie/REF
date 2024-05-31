b7acf269020d3ef765dd2973e07eb05a2701405d
zxcvb2002 <jbyjby3743@naver.com>
2024-06-01 02:23:16 +0900

    [fix] RecommendFragment Flow Implement

--- app/src/main/java/com/fsof/project/view/fragment/recommend/RecommendFragment.kt
+++ app/src/main/java/com/fsof/project/view/fragment/recommend/RecommendFragment.kt
@@ -17,6 +17,7 @@ import com.fsof.project.controller.RecipeController
 import com.fsof.project.controller.client.RecipeClient
 import com.fsof.project.model.entity.Ingredients
 import com.fsof.project.model.datasource.IngredientDatabase
+import com.fsof.project.model.entity.Recipes
 import com.fsof.project.model.repository.RecipeRepository
 
 class RecommendFragment : Fragment() {
@@ -27,13 +28,6 @@ class RecommendFragment : Fragment() {
     private lateinit var recipeController: RecipeController
     private lateinit var recipeRepository: RecipeRepository
     private lateinit var ingredientsDatabase: IngredientDatabase
-
-    private lateinit var morningIngredients: List<String>
-    private lateinit var morningDetailedInstructions: String
-    private lateinit var lunchIngredients: List<String>
-    private lateinit var lunchDetailedInstructions: String
-    private lateinit var dinnerIngredients: List<String>
-    private lateinit var dinnerDetailedInstructions: String
 
     private val handler = Handler(Looper.getMainLooper())
 
@@ -48,23 +42,38 @@ class RecommendFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // init
         ingredientsDatabase = IngredientDatabase.getInstance(requireContext())
         recipeRepository = RecipeRepository(RecipeClient.recipeService)
         recipeController = RecipeController(recipeRepository)
+        createRecipes(ingredientsDatabase.ingredientsDao().selectAll()) { recipes ->
+            recipes.let {
+                if (it != null) {
+                    updateUI(it)
+                    openDetails(it)
+                } else {
+                    Log.d("API", "recipes == null")
+                    activity?.let {
+                        Toast.makeText(it, "레시피 정보가 잘못되었어요. 다시 시도해주세요.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
 
-        createRecipes(ingredientsDatabase.ingredientsDao().selectAll())
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
+    private fun openDetails(recipe: Recipes) {
         binding.morningText.setOnClickListener {
-            navigateToDetail("아침 메뉴", morningIngredients, morningDetailedInstructions)
+            navigateToDetail("아침 메뉴", recipe.breakfast.ingredients, recipe.breakfast.detailedInstructions)
         }
-
         binding.noonText.setOnClickListener {
-            navigateToDetail("점심 메뉴", lunchIngredients, lunchDetailedInstructions)
+            navigateToDetail("점심 메뉴", recipe.lunch.ingredients, recipe.lunch.detailedInstructions)
         }
-
         binding.nightText.setOnClickListener {
-            navigateToDetail("저녁 메뉴", dinnerIngredients, dinnerDetailedInstructions)
+            navigateToDetail("저녁 메뉴", recipe.dinner.ingredients, recipe.dinner.detailedInstructions)
         }
     }
 
@@ -77,57 +86,55 @@ class RecommendFragment : Fragment() {
         startActivity(intent)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    private fun createRecipes(input: List<Ingredients>) {
-        recipeController.createRecipe(input) { recipes, throwable ->
+    private fun createRecipes(input: List<Ingredients>, callback: (Recipes?) -> Unit)/*: Recipes?*/ {
+        var recipe: Recipes? = null
+        recipeController.createRecipe(input) { results, throwable ->
             handler.post {
                 if (throwable != null) {
                     Log.d("API", "Error: ${throwable.message}")
                     activity?.let { Toast.makeText(it, "레시피 생성 실패 ㅠㅠ", Toast.LENGTH_SHORT).show() }
-                } else if (recipes != null) {
-                    Log.d("API", "Recipes: $recipes")
-
-                    // ImageView Disabled
-                    binding.morningImage.visibility = View.GONE
-                    binding.noonImage.visibility = View.GONE
-                    binding.nightImage.visibility = View.GONE
-
-                    // Align To Center
-                    val morningLayoutParams =
-                        binding.morningText.layoutParams as RelativeLayout.LayoutParams
-                    morningLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL)
-                    binding.morningText.layoutParams = morningLayoutParams
-                    val noonLayoutParams =
-                        binding.noonText.layoutParams as RelativeLayout.LayoutParams
-                    noonLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL)
-                    binding.noonText.layoutParams = noonLayoutParams
-                    val nightLayoutParams =
-                        binding.nightText.layoutParams as RelativeLayout.LayoutParams
-                    nightLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL)
-                    binding.nightText.layoutParams = nightLayoutParams
-
-                    Log.d("API", recipes.toString()) // binding.recipeText.text = recipes.toString() // binding.recipeText.setText(recipes.toString())
-
-                    // Binding
-                    binding.morningText.text = recipes.breakfast.name
-                    binding.noonText.text = recipes.lunch.name
-                    binding.nightText.text = recipes.dinner.name
-
-                    morningIngredients = recipes.breakfast.ingredients
-                    morningDetailedInstructions = recipes.breakfast.detailedInstructions
-                    lunchIngredients = recipes.breakfast.ingredients
-                    lunchDetailedInstructions = recipes.breakfast.detailedInstructions
-                    dinnerIngredients = recipes.breakfast.ingredients
-                    dinnerDetailedInstructions = recipes.breakfast.detailedInstructions
+                    callback(null)
                 } else {
-                    Log.d("API", "Unknown error occurred")
-                    activity?.let { Toast.makeText(it, "레시피 생성 실패 ㅠㅠ", Toast.LENGTH_SHORT).show() }
+                    results?.let {
+                        Log.d("API", "Recipes: $it")
+                        recipe = it
+                        callback(it)
+                    } ?: run {
+                        activity?.let {
+                            Toast.makeText(it, "레시피 생성 실패 ㅠㅠ", Toast.LENGTH_SHORT).show()
+                        }
+                        callback(null)
+                    }
+                    Log.d("API", recipe.toString()) // binding.recipeText.text = recipes.toString() // binding.recipeText.setText(recipes.toString())
                 }
             }
         }
+        // return recipe
+    }
+
+    private fun updateUI(recipe: Recipes) {
+        // ImageView Disabled
+        binding.morningImage.visibility = View.GONE
+        binding.noonImage.visibility = View.GONE
+        binding.nightImage.visibility = View.GONE
+
+        // Align To Center
+        val morningLayoutParams =
+            binding.morningText.layoutParams as RelativeLayout.LayoutParams
+        morningLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL)
+        binding.morningText.layoutParams = morningLayoutParams
+        val noonLayoutParams =
+            binding.noonText.layoutParams as RelativeLayout.LayoutParams
+        noonLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL)
+        binding.noonText.layoutParams = noonLayoutParams
+        val nightLayoutParams =
+            binding.nightText.layoutParams as RelativeLayout.LayoutParams
+        nightLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL)
+        binding.nightText.layoutParams = nightLayoutParams
+
+        // Binding
+        binding.morningText.text = recipe.breakfast.name
+        binding.noonText.text = recipe.lunch.name
+        binding.nightText.text = recipe.dinner.name
     }
 }
